d99f7b17f5ba5272040c699cd72467ffd94a4f34
ghifarozarrr <ghifaroza16@mhs.if.its.ac.id>
2019-05-21 16:04:48 +0700

    add head, options

--- tugas-5/http.py
+++ tugas-5/http.py
@@ -12,44 +12,73 @@ class HttpServer:
 		self.types['.jpg']='image/jpeg'
 		self.types['.txt']='text/plain'
 		self.types['.html']='text/html'
+
 	def response(self,kode=404,message='Not Found',messagebody='',headers={}):
 		tanggal = datetime.now().strftime('%c')
 		resp=[]
 		resp.append("HTTP/1.0 {} {}\r\n" . format(kode,message))
 		resp.append("Date: {}\r\n" . format(tanggal))
-		resp.append("Connection: close\r\n")
+		if (self.method1!='OPTIONS'):
+			resp.append("Connection: close\r\n")
+			resp.append("Content-Length: {}\r\n" . format(len(messagebody)))
 		resp.append("Server: myserver/1.0\r\n")
-		resp.append("Content-Length: {}\r\n" . format(len(messagebody)))
 		for kk in headers:
 			resp.append("{}:{}\r\n" . format(kk,headers[kk]))
 		resp.append("\r\n")
-		resp.append("{}" . format(messagebody))
+		if (self.method1!='HEAD'):
+			resp.append("{}" . format(messagebody))
 		response_str=''
 		for i in resp:	
 			response_str="{}{}" . format(response_str,i)
 		return response_str
 
 	def proses(self,data):
-		
 		requests = data.split("\r\n")
 		baris = requests[0]
 
 		print baris
 		j = baris.split(" ")
 		try:
-			method=j[0].upper().strip()
+			method = j[0].upper().strip()
+			self.method1 = method 
 			if (method=='GET'):
 				object_address = j[1].strip()
 				return self.http_get(object_address)
+			elif (method=='HEAD'):
+				object_address = j[1].strip()
+				return self.http_head(object_address)
+			elif (method=='OPTIONS'):
+				object_address = j[1].strip()
+				return self.http_options(object_address)
+			elif (method=='POST'):
+				object_address = j[1].strip()
+				return self.http_post(object_address)
 			else:
 				return self.response(400,'Bad Request','',{})
 		except IndexError:
 			return self.response(400,'Bad Request','',{})
+
 	def http_get(self,object_address):
 		files = glob('./*')
 		thedir='.'
-		import string
-		object_address = string.replace(object_address, '/', '\\')
+
+		if thedir+object_address not in files:
+			return self.response(404,'Not Found','',{})
+		fp = open(thedir+object_address,'r')
+		isi = fp.read()
+		
+		fext = os.path.splitext(thedir+object_address)[1]
+		content_type = self.types[fext]
+		
+		headers={}
+		headers['Content-type'] = content_type
+		
+		return self.response(200,'OK',isi,headers)
+
+	def http_head(self, object_address):
+		files = glob('./*')
+		thedir='.'
+
 		if thedir+object_address not in files:
 			return self.response(404,'Not Found','',{})
 		fp = open(thedir+object_address,'r')
@@ -62,6 +91,22 @@ class HttpServer:
 		headers['Content-type']=content_type
 		
 		return self.response(200,'OK',isi,headers)
+
+	def http_options(self, object_address):
+		files = glob('./*')
+		thedir='.'
+
+		if thedir+object_address not in files:
+			return self.response(404,'Not Found','',{})
+
+		fext = os.path.splitext(thedir+object_address)[1]
+		content_type = self.types[fext]
+		
+		headers={}
+		headers['Content-type']=content_type
+		headers['Allow']='GET,HEAD,POST,OPTIONS'
+		
+		return self.response(200,'OK','',headers)
 
 if __name__=="__main__":
 	httpserver = HttpServer()
--- tugas-5/lb.py
+++ tugas-5/lb.py
@@ -66,7 +66,6 @@ class Server(asyncore.dispatcher):
 			handler.backend = backend
 
 
-
 def main():
 	svr = Server()
 	asyncore.loop()
